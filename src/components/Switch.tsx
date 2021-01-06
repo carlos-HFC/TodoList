6d8dbe479935da83e85764f031a20db44c3400d6
Carlos Faustino <chfcchfc96@gmail.com>
2021-01-06 11:26:08 -0300

    styled-components no lugar de className

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -6,64 +6,67 @@ const Container = styled.label`
    display: inline-block;
    width: 50px;
    height: 15px;
+`
 
-   input {
-      opacity: 0;
-      width: 0;
-      height: 0;
-
-      &:checked + .slider-round {
-         background: #000;
-
-         &::before {
-            transform: translateX(30px);
-         }
-      }
-
-      &:focus + .slider-round {
-         box-shadow: 0 0 1px #000
-      }
-   }
-
-   .slider-round {
-      background: #fff;
+const SliderRound = styled.span`
+   background: #fff;
+   position: absolute;
+   cursor: pointer;
+   top: 0;
+   bottom: 0;
+   right: 0;
+   left: 0;
+   transition: .2s;
+   border-radius: 30px;
+   
+   &::before {
       position: absolute;
-      cursor: pointer;
-      top: 0;
-      bottom: 0;
-      right: 0;
+      content: "";
+      height: 22px;
+      width: 22px;
       left: 0;
+      bottom: -3px;
+      background: #aaa;
       transition: .2s;
-      border-radius: 30px;
-      
-      &::before {
-         position: absolute;
-         content: "";
-         height: 22px;
-         width: 22px;
-         left: 0;
-         bottom: -3px;
-         background: #aaa;
-         transition: .2s;
-         border-radius: 50%;
-      }
+      border-radius: 50%;
    }
 `
 
-interface Props {
-   handleTheme(theme: string): void
+const Check = styled.input`
+   opacity: 0;
+   width: 0;
+   height: 0;
+
+   &:checked + ${SliderRound} {
+      background: #000;
+
+      &::before {
+         transform: translateX(30px)
+      }
+   }
+
+   &:focus + ${SliderRound} {
+      box-shadow: 0 0 1px #000
+   }
+`
+
+
+interface SwitchProps {
+   handleTheme: (theme: string) => void
 }
 
-export default function Switch({ handleTheme }: Props) {
+const Switch: React.FC<SwitchProps> = ({ handleTheme }) => {
    const { title } = useContext(ThemeContext)
 
    return (
-      <Container>
+      <Container title={`Modo ${title}`}>
          {title === 'light'
-            ? <input type="checkbox" onClick={() => handleTheme('light')} />
-            : <input type="checkbox" onClick={() => handleTheme('dark')} defaultChecked />
+            ? <Check type="checkbox" onClick={() => handleTheme('light')} />
+            : <Check type="checkbox" onClick={() => handleTheme('dark')} defaultChecked />
          }
-         <span className="slider-round" />
+         <SliderRound />
       </Container>
    )
 }
+
+export default Switch
