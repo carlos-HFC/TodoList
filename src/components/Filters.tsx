698f1d6ef0f96e5029b2df5bba5e1e94db6baadc
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-05 09:48:47 -0300

    Botão de cancelar filtros

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,100 +1,112 @@
 import { FormEvent, useState } from "react"
-import { FaArrowCircleDown, FaFilter } from "react-icons/fa"
+import { FaArrowCircleDown, FaBan, FaFilter } from "react-icons/fa"
 import styled from "styled-components"
 
 import { Button, RadioButton } from '.'
 
-interface FilterTodoProps {
-   filtrarTodo: (value: string, type: string) => void
-   disabled: boolean
-}
-
 const Collapse = styled.div<{ open: boolean }>`
-   overflow: hidden;
-   width: 75%;
-   margin: auto;
-   
-   @media screen and (max-width: 767px) {
-      width: 100%
-   }
+  margin: auto;
+  overflow: hidden;
+  width: 75%;
+  
+  @media screen and (max-width: 767px) {
+    width: 100%
+  }
 
-   ${props => {
-      if (props.open) {
-         return `
-            max-height: 500px;
-            transition: max-height 1s;
-         `
-      } else {
-         return `
-            max-height: 0;
-            transition: .3s;
-         `
-      }
-   }}
+  ${props => {
+    if (props.open) {
+      return `
+        max-height: 500px;
+        transition: max-height 1s;
+      `
+    } else {
+      return `
+        max-height: 0;
+        transition: .3s;
+      `
+    }
+  }}
 `
 
 const Icon = styled(FaArrowCircleDown) <{ open: boolean }>`
-   transition: .5s;
-   margin-right: .5rem;
-   transform: ${props => props.open ? 'rotate(180deg)' : 'rotate(0deg)'};
+  margin-right: .5rem;
+  transform: ${props => props.open ? 'rotate(180deg)' : 'rotate(0deg)'};
+  transition: .5s;
 `
 
-const Filters: React.FC<FilterTodoProps> = ({ filtrarTodo, disabled }) => {
-   const [value, setValue] = useState('')
-   const [type, setType] = useState('All')
-   const [open, setOpen] = useState(false)
+interface FilterTodoProps {
+  cancelFilter: () => void
+  disabledCancel: boolean
+  disabledFilter: boolean
+  filtrarTodo: (value: string, type: string) => void
+}
 
-   function filtrar(e: FormEvent) {
-      e.preventDefault()
-      filtrarTodo(value, type)
-   }
+const Filters: React.FC<FilterTodoProps> = ({ cancelFilter, filtrarTodo, disabledCancel, disabledFilter }) => {
+  const [open, setOpen] = useState(false)
+  const [type, setType] = useState('All')
+  const [value, setValue] = useState('')
 
-   return (
-      <section className="mb-4">
-         <div className="d-flex justify-content-center align-items-center">
-            <button onClick={() => setOpen(open => !open)} className="btn btn-primary" title="Utilizar filtros" aria-controls="collapse">
-               <Icon open={open} />Filtros
-            </button>
-         </div>
-         <Collapse open={open} id="collapse">
-            <form onSubmit={filtrar} className="row mb-3">
-               <div className="col-lg-6 mb-2" title="Filtrar pelo título da atividade">
-                  <label htmlFor="todoTitle">Título</label>
-                  <input type="text" id="todoTitle" className="form-control"
-                     value={value} onChange={e => setValue(e.target.value)}
-                  />
-               </div>
-               <fieldset className="form-group col-lg-4 col-12 mb-2">
-                  <legend className="col-form-label">Tipo</legend>
-                  <div className="row">
-                     <div className="col-12">
-                        <RadioButton id="All" name="FiltrarTodo" title="Filtrar todas as atividades"
-                           value="All" onChange={e => setType(e.target.value)} checked={type === 'All'}
-                        >
-                           All
-                        </RadioButton>
-                        <RadioButton id="Todo" name="FiltrarTodo" title="Filtrar atividades a fazer"
-                           value="Todo" onChange={e => setType(e.target.value)} checked={type === 'Todo'}
-                        >
-                           To Do
-                        </RadioButton>
-                        <RadioButton id="Done" name="FiltrarTodo" title="Filtrar atividades concluídas"
-                           value="Done" onChange={e => setType(e.target.value)} checked={type === 'Done'}
-                        >
-                           Done
-                        </RadioButton>
-                     </div>
-                  </div>
-               </fieldset>
-               <div className="col-lg col-12 mb-2 d-flex align-items-end justify-content-end">
-                  <Button background="primary" className="my-query__btn" disabled={disabled} title="Filtrar atividades">
-                     <FaFilter className="mr-2" />Filtrar
-                  </Button>
-               </div>
-            </form>
-         </Collapse>
-      </section>
-   )
+  function filtrar(e: FormEvent) {
+    e.preventDefault()
+    filtrarTodo(value, type)
+  }
+
+  function cancel() {
+    cancelFilter()
+    setOpen(false)
+    setType('All')
+    setValue('')
+  }
+
+  return (
+    <section className="mb-4">
+      <div className="d-flex justify-content-center align-items-center">
+        <button onClick={() => setOpen(open => !open)} className="btn btn-primary" title="Utilizar filtros" aria-controls="collapse">
+          <Icon open={open} />Filtros
+        </button>
+      </div>
+      <Collapse open={open} id="collapse">
+        <form onSubmit={filtrar} className="row mb-3">
+          <div className="col-lg-5 col-12 mb-2" title="Filtrar pelo título da atividade">
+            <label htmlFor="todoTitle">Título</label>
+            <input type="text" id="todoTitle" className="form-control"
+              value={value} onChange={e => setValue(e.target.value)}
+            />
+          </div>
+          <fieldset className="form-group col-lg-4 col-12 mb-2">
+            <legend className="col-form-label">Tipo</legend>
+            <div className="row">
+              <div className="col-12">
+                <RadioButton id="All" name="FiltrarTodo" title="Filtrar todas as atividades"
+                  value="All" onChange={e => setType(e.target.value)} checked={type === 'All'}
+                >
+                  All
+                </RadioButton>
+                <RadioButton id="Todo" name="FiltrarTodo" title="Filtrar atividades a fazer"
+                  value="Todo" onChange={e => setType(e.target.value)} checked={type === 'Todo'}
+                >
+                  To Do
+                </RadioButton>
+                <RadioButton id="Done" name="FiltrarTodo" title="Filtrar atividades concluídas"
+                  value="Done" onChange={e => setType(e.target.value)} checked={type === 'Done'}
+                >
+                  Done
+                </RadioButton>
+              </div>
+            </div>
+          </fieldset>
+          <div className="col-lg col-12 mb-2 d-flex align-items-end justify-content-end">
+            <Button background="primary" type="submit" className="my-query__btn btn-sm" disabled={disabledFilter} title="Filtrar atividades">
+              <FaFilter className="mr-2" />Filtrar
+            </Button>
+            <Button background="secondary" type="reset" className="my-query__btn btn-sm ml-2" disabled={disabledCancel} title="Cancelar filtros" onClick={cancel}>
+              <FaBan className="mr-2" />Cancelar
+            </Button>
+          </div>
+        </form>
+      </Collapse>
+    </section>
+  )
 }
 
 export default Filters
--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -3,68 +3,68 @@ import { useEffect, useState } from 'react'
 import { AddItem, Filters, ListTodo } from '../components'
 
 export default function Todo() {
-   const [todos, setTodos] = useState<string[]>(JSON.parse(localStorage.getItem("TODO") as string) || [])
-   const [checked, setChecked] = useState<number[]>(JSON.parse(localStorage.getItem("DONE") as string) || [])
-   const [todoFilter, setTodoFilter] = useState<string[]>([])
+  const [checked, setChecked] = useState<number[]>(JSON.parse(localStorage.getItem("DONE") as string) || [])
+  const [todos, setTodos] = useState<string[]>(JSON.parse(localStorage.getItem("TODO") as string) || [])
+  const [todoFilter, setTodoFilter] = useState<string[]>([])
 
-   useEffect(() => localStorage.setItem("TODO", JSON.stringify(todos)), [todos])
-   useEffect(() => localStorage.setItem("DONE", JSON.stringify(checked)), [checked])
+  useEffect(() => localStorage.setItem("TODO", JSON.stringify(todos)), [todos])
+  useEffect(() => localStorage.setItem("DONE", JSON.stringify(checked)), [checked])
 
-   function addItem(todo: string) {
-      let filtered = todos.filter(el => todo === el)
+  function addItem(todo: string) {
+    let filtered = todos.filter(el => todo === el)
 
-      if (todos.length > 0 && filtered.length > 0) {
-         return "Erro"
+    if (todos.length > 0 && filtered.length > 0) {
+      return "Erro"
+    } else {
+      setTodos([...todos, todo])
+      return "Sucesso"
+    }
+  }
+
+  function removeItem(todo: string) {
+    let filtered = todos.filter(el => todo !== el)
+    setTodos(filtered)
+  }
+
+  function handleDone(id: number) {
+    const already = checked.findIndex(el => el === id)
+
+    if (already >= 0) {
+      const filtered = checked.filter(el => el !== id)
+      setChecked(filtered.sort())
+    } else {
+      setChecked([...checked, id].sort())
+    }
+  }
+
+  function filtrarTodo(value: string, type: string) {
+    let filtered = todos
+
+    filtered = filtered.filter((item, i) => {
+      if (value) {
+        if (type === 'Done') return checked.includes(i) && item.toLowerCase().startsWith(value.toLowerCase())
+        else if (type === 'Todo') return !checked.includes(i) && item.toLowerCase().startsWith(value.toLowerCase())
+        else return item.toLowerCase().startsWith(value.toLowerCase())
       } else {
-         setTodos([...todos, todo])
-         return "Sucesso"
+        if (type === 'Done') return checked.includes(i)
+        else if (type === 'Todo') return !checked.includes(i)
+        else return item
       }
-   }
+    })
 
-   function removeItem(todo: string) {
-      let filtered = todos.filter(el => todo !== el)
-      setTodos(filtered)
-   }
+    if (filtered.length === 0) filtered = ["Error404\\NotFound"]
 
-   function handleDone(id: number) {
-      const already = checked.findIndex(el => el === id)
+    setTodoFilter(filtered)
+  }
 
-      if (already >= 0) {
-         const filtered = checked.filter(el => el !== id)
-         setChecked(filtered.sort())
-      } else {
-         setChecked([...checked, id].sort())
-      }
-   }
-
-   function filtrarTodo(value: string, type: string) {
-      let filtered = todos
-
-      filtered = filtered.filter((item, i) => {
-         if (value) {
-            if (type === 'Done') return checked.includes(i) && item.toLowerCase().startsWith(value.toLowerCase())
-            else if (type === 'Todo') return !checked.includes(i) && item.toLowerCase().startsWith(value.toLowerCase())
-            else return item.toLowerCase().startsWith(value.toLowerCase())
-         } else {
-            if (type === 'Done') return checked.includes(i)
-            else if (type === 'Todo') return !checked.includes(i)
-            else return item
-         }
-      })
-
-      if (filtered.length === 0) filtered = ["Error404\\NotFound"]
-
-      setTodoFilter(filtered)
-   }
-
-   return (
-      <main className="m-3">
-         <div className="container p-3">
-            <h2 className="text-center mb-3" title="Insira suas atividades">Insira suas atividades</h2><hr />
-            <AddItem add={addItem} />
-            <Filters filtrarTodo={filtrarTodo} disabled={!todos.length} />
-            <ListTodo todos={todos} remove={removeItem} done={checked} handleDone={handleDone} todoFilter={todoFilter} />
-         </div>
-      </main>
-   )
+  return (
+    <main className="m-3">
+      <div className="container p-3">
+        <h2 className="text-center mb-3" title="Insira suas atividades">Insira suas atividades</h2><hr />
+        <AddItem add={addItem} />
+        <Filters filtrarTodo={filtrarTodo} disabledCancel={!todoFilter.length} disabledFilter={!todos.length} cancelFilter={() => setTodoFilter([])} />
+        <ListTodo todos={todos} remove={removeItem} done={checked} handleDone={handleDone} todoFilter={todoFilter} />
+      </div>
+    </main>
+  )
 }
