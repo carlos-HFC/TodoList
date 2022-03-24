1ed0ba8855cbf70c84c5d8b7af7be4c240b2b219
Carlos Faustino <chfcchfc96@gmail.com>
2022-03-24 16:48:14 -0300

    Filtrar, editar todos, componente para filtro e listar todos filtradas

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,38 +1,40 @@
+import { lazy, Suspense, useRef } from "react";
 import { FormEvent, useEffect, useState } from "react";
+import { VscListSelection, VscSave } from "react-icons/vsc";
 
-import { Button, Input, Item } from "../../components";
+import { Button, Input } from "../../components";
 
-import { List, Main } from "./styles";
+import { Filter, List, Main } from "./styles";
 
-interface Todos {
+const Item = lazy(() => import('../../components').then(({ Item }) => ({ default: Item })));
+
+interface TTodos {
   id: number;
   text: string;
   checked: boolean;
 }
 
+interface TFilterTodos {
+  filter: boolean;
+  todos: TTodos[];
+}
+
+const INITIAL_STATE_TODOS_FILTERED = {} as TFilterTodos;
+const INITIAL_STATE_EDIT_TODO = {} as TTodos;
+const INITIAL_STATE_FILTER_TODO = {
+  text: "",
+  type: ""
+};
+
 export function Todo() {
-  // function filtrarTodo(value: string, type: string) {
-  //   let filtered = todos;
+  const listRef = useRef<HTMLUListElement | null>(null);
 
-  //   filtered = filtered.filter((item, i) => {
-  //     if (value) {
-  //       if (type === 'Done') return checked.includes(i) && item.toLowerCase().startsWith(value.toLowerCase());
-  //       else if (type === 'Todo') return !checked.includes(i) && item.toLowerCase().startsWith(value.toLowerCase());
-  //       else return item.toLowerCase().startsWith(value.toLowerCase());
-  //     } else {
-  //       if (type === 'Done') return checked.includes(i);
-  //       else if (type === 'Todo') return !checked.includes(i);
-  //       else return item;
-  //     }
-  //   });
-
-  //   if (filtered.length === 0) filtered = ["Error404\\NotFound"];
-
-  //   setTodoFilter(filtered);
-  // }
-
+  const [openFilter, setOpenFilter] = useState(false);
   const [item, setItem] = useState("");
-  const [todos, setTodos] = useState<Todos[]>(JSON.parse(localStorage.getItem(String(process.env.REACT_APP_TODO_LIST)) as string) || []);
+  const [edit, setEdit] = useState(INITIAL_STATE_EDIT_TODO);
+  const [filter, setFilter] = useState(INITIAL_STATE_FILTER_TODO);
+  const [todos, setTodos] = useState<TTodos[]>(JSON.parse(localStorage.getItem(String(process.env.REACT_APP_TODO_LIST)) as string) || []);
+  const [todosFiltered, setTodosFiltered] = useState(INITIAL_STATE_TODOS_FILTERED);
 
   useEffect(() => localStorage.setItem(String(process.env.REACT_APP_TODO_LIST), JSON.stringify(todos)), [todos]);
 
@@ -57,11 +59,58 @@ export function Todo() {
   function handleToggleChecked(id: number) {
     const filtered = todos.filter(todo => todo.id !== id);
 
-    const todo = todos.find(todo => todo.id === id) as Todos;
+    const todoById = todos.find(todo => todo.id === id) as TTodos;
 
-    Object.assign(todo, { checked: !todo.checked });
+    Object.assign(todoById, { checked: !todoById.checked });
 
-    setTodos([...filtered, todo].sort((a, b) => a.id - b.id));
+    setTodos([...filtered, todoById].sort((a, b) => a.id - b.id));
+  }
+
+  function editItem(id: number) {
+    const todoById = todos.find(todo => todo.id === id) as TTodos;
+
+    setEdit(todoById);
+  }
+
+  function handleSaveEdit(e: FormEvent) {
+    e.preventDefault();
+
+    const filtered = todos.filter(todo => todo.id !== edit.id);
+
+    const todoById = todos.find(todo => todo.id === edit.id) as TTodos;
+
+    Object.assign(todoById, { text: edit.text });
+
+    setTodos([...filtered, todoById].sort((a, b) => a.id - b.id));
+
+    setEdit(INITIAL_STATE_EDIT_TODO);
+  }
+
+  function handleFilterTodo(e: FormEvent) {
+    e.preventDefault();
+
+    const filtered = todos.filter(todo => {
+      if (filter.text) {
+        if (filter.type === "Done") return todo.checked && todo.text.toLowerCase().startsWith(filter.text.toLowerCase());
+        if (filter.type === "Incomplete") return !todo.checked && todo.text.toLowerCase().startsWith(filter.text.toLowerCase());
+        return todo.text.toLowerCase().startsWith(filter.text.toLowerCase());
+      } else {
+        if (filter.type === "Done") return todo.checked;
+        if (filter.type === "Incomplete") return !todo.checked;
+        return todo;
+      }
+    });
+
+    if (filtered.length > 0) {
+      listRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+
+    setTodosFiltered({ todos: filtered, filter: true });
+  }
+
+  function cancelFilterTodo() {
+    setTodosFiltered(INITIAL_STATE_TODOS_FILTERED);
+    setFilter(INITIAL_STATE_FILTER_TODO);
   }
 
   return (
@@ -71,16 +120,79 @@ export function Todo() {
 
         <form onSubmit={addItem}>
           <Input value={item} onChange={e => setItem(e.target.value)} />
-          <Button variant="success">
+          <Button variant="success" disabled={!item.trim()}>
             Adicionar
           </Button>
         </form>
 
-        <List>
-          {todos.map(todo => (
-            <Item key={todo.id} checked={todo.checked} onChecked={() => handleToggleChecked(todo.id)} onDelete={() => removeItem(todo.id)}>
-              {todo.text}
-            </Item>
+        <Filter open={openFilter}>
+          <header>
+            <h2>Filtrar</h2>
+            <Button onClick={() => setOpenFilter(open => !open)}>
+              <VscListSelection />
+            </Button>
+          </header>
+
+          <div className="filter">
+            <form onSubmit={handleFilterTodo}>
+              <Input label="Título To Do"
+                value={filter.text} onChange={e => setFilter({ ...filter, text: e.target.value })}
+              />
+              <fieldset>
+                <span>Tipo</span>
+                <div className="filter_types">
+                  <div className="filter_types-check">
+                    <input type="radio" id="All" name="filterTodos"
+                      checked={!filter.type} value="" onChange={e => setFilter({ ...filter, type: e.target.value })}
+                    />
+                    <label htmlFor="All">Todos</label>
+                  </div>
+                  <div className="filter_types-check">
+                    <input type="radio" id="Incomplete" name="filterTodos"
+                      checked={filter.type === "Incomplete"} value="Incomplete" onChange={e => setFilter({ ...filter, type: e.target.value })}
+                    />
+                    <label htmlFor="Incomplete">A fazer</label>
+                  </div>
+                  <div className="filter_types-check">
+                    <input type="radio" id="Done" name="filterTodos"
+                      checked={filter.type === "Done"} value="Done" onChange={e => setFilter({ ...filter, type: e.target.value })}
+                    />
+                    <label htmlFor="Done">Concluídos</label>
+                  </div>
+                </div>
+              </fieldset>
+              <div className="filter_btns">
+                <Button type="submit">
+                  Filtrar
+                </Button>
+                <Button variant="secondary" onClick={cancelFilterTodo} type="reset">
+                  Cancelar
+                </Button>
+              </div>
+            </form>
+          </div>
+        </Filter>
+
+        <List ref={listRef}>
+          {(todosFiltered.todos || todos).map(todo => edit.id === todo.id ? (
+            <form onSubmit={handleSaveEdit}>
+              <input type="text"
+                defaultValue={edit.text} onChange={e => setEdit({ ...edit, text: e.target.value })}
+              />
+              <Button>
+                <VscSave />
+              </Button>
+            </form>
+          ) : (
+            <Suspense fallback={"ola"}>
+              <Item key={todo.id} checked={todo.checked}
+                onChecked={() => handleToggleChecked(todo.id)}
+                onEdit={() => editItem(todo.id)}
+                onDelete={() => removeItem(todo.id)}
+              >
+                {todo.text}
+              </Item>
+            </Suspense>
           ))}
         </List>
       </div>
--- src/pages/Todo/styles.ts
+++ src/pages/Todo/styles.ts
@@ -1,4 +1,7 @@
+import { lighten } from "polished";
 import styled from "styled-components";
+
+import { COLORS } from "../../css/themes/variables";
 
 export const Main = styled.main`
   padding: 3rem 0;
@@ -11,7 +14,7 @@ export const Main = styled.main`
 
   h1 {
     color: ${props => props.theme.text};
-    border-bottom: 1px solid ${props => props.theme.text};
+    border-bottom: 1px solid ${props => lighten(.5, props.theme.text)};
     text-align: center;
     padding-bottom: 1rem;
     transition: color .3s, border-color .3s;
@@ -34,6 +37,149 @@ export const Main = styled.main`
   }
 `;
 
+export const Filter = styled.section<{ open: boolean; }>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+
+  header {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+
+    @media (min-width: 768px) {
+      width: 80%;
+    }
+
+    h2 {
+      color: ${props => props.theme.text};
+      font-size: 1.25rem;
+    }
+
+    button {
+      border-radius: .25rem;
+      padding: .5rem;
+    }
+  }
+
+  .filter {
+    max-height: 0;
+    overflow: hidden;
+    transition: max-height .5s;
+    width: 100%;
+    ${props => props.open && `
+      max-height: 500px;
+      transition: max-height 1.5s;
+    `};
+    
+    @media (min-width: 768px) {
+      width: 80%;
+    }
+
+    form {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      margin: 1rem 0;
+
+      @media (min-width: 768px) {
+        flex-direction: row;
+      }
+
+      input[type="text"] {
+        box-shadow: inset 0 0 0 0.15rem ${lighten(.7, COLORS.black)};
+      }
+
+      fieldset {
+        display: flex;
+        justify-content: center;
+        gap: 1rem;
+        border: 0;
+        flex-direction: column;
+        width: 100%;
+
+        @media (min-width: 768px) {
+          width: 70%;
+        }
+
+        span {
+          color: ${props => props.theme.text};
+        }
+
+        .filter_types{
+          display: flex;
+          gap: 1rem;
+          flex-direction: column;
+
+          @media (min-width: 768px) {
+            flex-direction: row;
+          }
+
+          &-check {
+            display: flex;
+            flex-direction: row;
+            gap: 0.5rem;
+            align-items: center;
+            width: auto;
+
+            input[type="radio"] {
+              appearance: none;
+              border: 1px solid ${COLORS.gray400};
+              outline: none;
+              position: relative;
+              transition: background-color .2s, border-color .2s;
+              border-radius: 50%;
+              height: 1rem;
+              width: 1rem;
+              min-width: 1rem;
+              max-width: 1rem;
+              min-height: 1rem;
+              max-height: 1rem;
+              user-select: none;
+
+              &:checked {
+                background: ${COLORS.gray600};
+                border-color: ${COLORS.gray600};
+              }
+            }
+
+            label {
+              color: ${props => props.theme.text};
+            }
+          }
+        }
+      }
+
+      .filter_btns {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        gap: 0.5rem;
+
+        @media (min-width: 768px) {
+          flex-direction: row;
+          width: auto;
+        }
+        
+        button {
+          width: 100%;
+          
+          @media (min-width: 768px) {
+            width: auto;
+          }
+        }
+      }
+    }
+  }
+`;
+
 export const List = styled.ul`
   display: flex;
   justify-content: center;
@@ -41,4 +187,49 @@ export const List = styled.ul`
   align-items: center;
   gap: 1rem;
   width: 100%;
+
+  form {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border: 2px solid ${props => props.theme.list.border};
+    border-radius: 0.5rem;
+    width: 100%;
+    padding: .675rem 1rem;
+    box-shadow: 0 1px 2px 1px ${props => props.theme.shadow};
+    background: ${props => props.theme.background};
+    transition: box-shadow .3s, border-color .3s, background-color .3s;
+    position: relative;
+    gap: 1rem;
+    flex-direction: row;
+
+    @media (min-width: 992px) {
+      width: 80%;
+    }
+
+    input {
+      height: 100%;
+      appearance: none;
+      line-height: 1.5;
+      border-radius: .25rem;
+      border: 0;
+      padding: .25rem 1rem;
+      font-size: 1rem;
+      color: ${props => props.theme.text};
+      background: transparent;
+      box-shadow: 0 0 0 .15rem ${lighten(.7, COLORS.black)};
+      outline: none;
+      width: 100%;
+
+      @media (min-width: 768px) {
+        width: 80%;
+      }
+    }
+
+    button {
+      padding: 0.5rem;
+      font-size: .8rem;
+      border-radius: .25rem
+    }
+  }
 `;
