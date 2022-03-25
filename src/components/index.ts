f16bab7a7d71ab7bd3ccac1e03bc7f711ba1b031
Carlos Faustino <chfcchfc96@gmail.com>
2022-03-25 13:14:36 -0300

    Exclusão de código desnecessário

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,19 +1,5 @@
-import AddItem from './AddItem';
-import Filters from './Filters';
-import Json from './Json';
-import ListTodo from './ListTodo';
-import Notification from './Notification';
-import RadioButton from './RadioButton';
 export * from './Button';
+export * from './Filter';
 export * from './Header';
 export * from './Input';
 export * from './Item';
-
-export {
-  AddItem,
-  Filters,
-  Json,
-  Notification,
-  ListTodo,
-  RadioButton,
-};
