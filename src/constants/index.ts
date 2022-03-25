20f6307fa269fa19d9acaba9370bb5fd9de32a26
Carlos Faustino <chfcchfc96@gmail.com>
2022-03-25 13:16:36 -0300

    Constantes compartilhadas

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+import { TFilterTodos, TTodos } from "../@types";
+
+export const INITIAL_STATE_EDIT_TODO = {} as TTodos;
+export const INITIAL_STATE_TODOS_FILTERED = {} as TFilterTodos;
+export const INITIAL_STATE_FILTER_TODO = {
+  text: "",
+  type: ""
+};
